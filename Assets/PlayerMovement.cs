cfa005b66f2d8d457782b39dc7055bd1cf38a267
Justin Yu <Ytki86@gmail.com>
2019-04-24 15:52:38 -0700

    first project upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSPeed = 20f;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSPeed;
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
